f68d35527c42de78572f04839ae7771707575431
unknown <contactmithun.sh@gmail.com>
2024-08-28 18:08:37 +0530

    firstcommit

---
+++ JS/task.js
@@ -0,0 +1,20 @@
+console.log(5+5);
+console.log(typeof console);
+
+// BOM
+console.log(prompt("helloooo class"));
+// alert("mithun is a  bad boy")
+// confirm("yes he is not a cute boy")
+
+console.log(typeof 'hai');
+console.log(typeof String);
+console.log(typeof 100);
+console.log(typeof Number);
+console.log(typeof true);
+console.log(typeof Boolean);
+console.log(typeof null);
+console.log(typeof 1n);
+console.log(typeof BigInt);
+console.log(typeof undefined);
+console.log(Symbol(1)==Symbol(1));
+console.log('hello');
